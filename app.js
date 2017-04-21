c0a9af5ec855320662049ce58df51e498c859082
Kyle Montierth <monty87@gmail.com>
2017-04-20 20:21:59 -0600

    console logs in app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 alert('Welcome to Git !');
+
+console.log('We are on a new branch!');
+
+var firstName = 'Kyle';
+var lastName = 'Montierth';
+
+console.log('Full Name: ' + firstName + ' ' + lastName);
