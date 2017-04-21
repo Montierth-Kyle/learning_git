b034168477411e29d99f8cae95f46b26c958b7af
Kyle Montierth <monty87@gmail.com>
2017-04-20 20:30:39 -0600

    age and if else

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ console.log('We are on a new branch!');
 
 var firstName = 'Kyle';
 var lastName = 'Montierth';
+var age = '29';
 
 console.log('Full Name: ' + firstName + ' ' + lastName);
+
+if(age > 19)
+    console.log('Not a teen. ')
+else
+    console.log('We are a teen. We hate our parents. ')
